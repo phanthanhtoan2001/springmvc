7d42740f2e43250ba6207310496425b6114adf4c
NSinh <suppersinh1@gmail.com>
2023-02-23 14:33:18 +0700

    add cartcontroller

---
+++ src/main/java/com/laptrinhjavaweb/controller/CartController.java
@@ -0,0 +1,27 @@
+package com.laptrinhjavaweb.controller;
+import com.laptrinhjavaweb.model.*;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+@Controller
+@RequestMapping("/Cart")
+public class CartController {
+	static String db_name = "dbwebflower", db_collection = "User";
+}
