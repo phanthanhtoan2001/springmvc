819072484a879acd17aa38673b91f67b339f2a4d
NSinh <suppersinh1@gmail.com>
2023-02-23 16:17:11 +0700

    add flower model, item model

--- src/main/java/com/laptrinhjavaweb/controller/CartController.java
+++ src/main/java/com/laptrinhjavaweb/controller/CartController.java
@@ -24,4 +24,50 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/Cart")
 public class CartController {
 	static String db_name = "dbwebflower", db_collection = "User";
+	
+
+	@RequestMapping(value = "index", method = RequestMethod.GET)
+	public String index() {
+		return "cart/index";
+	}
+
+	@RequestMapping(value = "buy/{id}", method = RequestMethod.GET)
+	public String buy(@PathVariable("id") String id, HttpSession session) {
+		ProductModel productModel = new ProductModel();
+		if (session.getAttribute("cart") == null) {
+			List<Item> cart = new ArrayList<Item>();
+			cart.add(new Item(productModel.find(id), 1));
+			session.setAttribute("cart", cart);
+		} else {
+			List<Item> cart = (List<Item>) session.getAttribute("cart");
+			int index = this.exists(id, cart);
+			if (index == -1) {
+				cart.add(new Item(productModel.find(id), 1));
+			} else {
+				int quantity = cart.get(index).getQuantity() + 1;
+				cart.get(index).setQuantity(quantity);
+			}
+			session.setAttribute("cart", cart);
+		}
+		return "redirect:/cart/index";
+	}
+
+	@RequestMapping(value = "remove/{id}", method = RequestMethod.GET)
+	public String remove(@PathVariable("id") String id, HttpSession session) {
+		ProductModel productModel = new ProductModel();
+		List<Item> cart = (List<Item>) session.getAttribute("cart");
+		int index = this.exists(id, cart);
+		cart.remove(index);
+		session.setAttribute("cart", cart);
+		return "redirect:/cart/index";
+	}
+
+	private int exists(String id, List<Item> cart) {
+		for (int i = 0; i < cart.size(); i++) {
+			if (cart.get(i).getProduct().getId().equalsIgnoreCase(id)) {
+				return i;
+			}
+		}
+		return -1;
+	}
 }
---
+++ src/main/java/com/laptrinhjavaweb/model/Flower.java
@@ -0,0 +1,5 @@
+package com.laptrinhjavaweb.model;
+
+public class Flower {
+
+}
